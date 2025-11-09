0b4796794ed9c2036f02a48c110d4a5bcf5aa3f8
edomac <edomac@gmail.com>
2025-11-09 23:28:57 +0700

    add c8

---
+++ c8_conditions.js
@@ -0,0 +1,13 @@
+hasPassport = false;
+if (hasPassport){
+    console.log('Kamu boleh keluar negeri');
+} else {
+    console.log('Kamu tidak boleh keluar negeri');
+}
+
+isSoldOut = true;
+if (isSoldOut== true){
+    console.log('Produk tersedia untuk dibeli');
+} else {
+    console.log('Produk tidak tersedia untuk dibeli');
+}
