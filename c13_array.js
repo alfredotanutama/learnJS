639f0a7f747566bf0ab76eea218861db3b5beed3
alfredotanutama <edo@gmail.com>
2025-11-14 00:08:48 +0700

    Add c_13

--- c13_array.js
+++ c13_array.js
@@ -1,2 +1,12 @@
 const usernames = ['yandy','dimas','mita'];
-console.log("Ada total "+ usernames.length+ " username.");
+// console.log("Ada total "+ usernames.length+ " username.");
+
+const newUserName = 'yandy';
+
+const isUserNameAvailable = usernames.includes(newUserName);
+
+if (isUserNameAvailable){
+    console.log("Username sudah digunakan")
+}else{
+    console.log("Username tersedia")
+}
